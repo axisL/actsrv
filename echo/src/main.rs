63623550021b0237624d9f4b4a356b9678f2cb67
axisL <34-axisL@users.noreply.git.hdpunk.com>
2021-06-21 11:28:15 +0800

    echo server use mini_redis

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6123").await.unwrap();
     println!("Hello, world!");
+    loop {
+        let (socket,_) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+    if let Some(frame)= connection.read_frame().await.unwrap(){
+        println!("receive: {:?}",frame);
+        let response:Frame = frame;
+        connection.write_frame(&response).await.unwrap();
+    }
 }
