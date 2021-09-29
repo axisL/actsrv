b255ccaa9cb3ed92445f9357bc43780fb96d9732
axisL <34-axisL@users.noreply.git.hdpunk.com>
2021-09-29 17:01:35 +0800

    client: support CLI parameters

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use tokio::net::TcpStream;
 use tokio::io::{Interest, AsyncWriteExt};
 use tokio_util::codec::{BytesCodec, FramedRead, FramedWrite};
-use std::io;
+use std::{env, io};
 use futures::{StreamExt, Stream, Sink};
 use futures::future;
 use std::net::SocketAddr;
@@ -11,32 +11,48 @@ use futures_util::SinkExt;
 
 
 #[tokio::main]
-async fn main() -> Result<(),Box<dyn Error>> {
-    let mut stream=TcpStream::connect("127.0.0.1:6123").await.expect("connect to server failed");
-    // send_once( stream).await;
-    send_continues(stream).await;
-    // write().await?;
-    // read_write().await?;
-    println!("main finish!");
+async fn main() -> Result<(), Box<dyn Error>> {
+    let stream = TcpStream::connect("127.0.0.1:6123").await.expect("connect to server failed");
+    let mode = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "c".to_string());
+    match mode.as_str() {
+        "o" => {
+            send_once(stream).await;
+        }
+        "c" => {
+            send_continues(stream).await?;
+        }
+        "w" => {
+            write().await?;
+        }
+        "rw" => {
+            read_write().await?;
+        }
+        _ => {
+            send_continues(stream).await?;
+        }
+    }
+    println!("fn main finished!");
     Ok(())
 }
 
-async fn send_continues(mut socket: TcpStream) -> Result<(),Box<dyn Error>> {
+async fn send_continues(mut socket: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut need_send = true;
-    loop{
+    loop {
         let ready = socket.ready(Interest::READABLE | Interest::WRITABLE).await?;
         if ready.is_readable() {
-            let mut data = vec![0;1024];
+            let mut data = vec![0; 1024];
             match socket.try_read(&mut data) {
-                Ok(n) =>{
-                    need_send =true;
-                    println!("read length: {:?} of data: {:?}",n,data);
+                Ok(n) => {
+                    need_send = true;
+                    println!("read length: {:?} of data: {:?}", n, data);
                 }
-                Err(e) if e.kind() == io::ErrorKind::WouldBlock =>{
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
                     println!("read block");
                     continue;
                 }
-                Err(e) =>{
+                Err(e) => {
                     return Err(e.into());
                 }
             }
@@ -47,29 +63,16 @@ async fn send_continues(mut socket: TcpStream) -> Result<(),Box<dyn Error>> {
             let to_send = b"wtf---0";
             socket.write_all(to_send).await?;
             println!("sent data: {:?}", to_send);
-            // match write {
-            //     Ok(n) => {
-            //         println!("sent data: {:?}", n);
-            //     }
-            //     Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-            //         println!("write block:");
-            //         continue
-            //     }
-            //     Err(e) => {
-            //         println!("error block: {:?}", e);
-            //         return Err(e.into());
-            //     }
-            // }
         } else {
-            continue
+            continue;
         }
     }
 }
 
 
-async fn send_once(mut socket:TcpStream) {
+async fn send_once(mut socket: TcpStream) {
     let result = socket.write(b"wtf").await;
-    println!("send ok: {:?}",result.is_ok());
+    println!("send ok: {:?}", result.is_ok());
     result.unwrap();
 }
 
@@ -85,7 +88,7 @@ async fn write() -> Result<(), Box<dyn Error>> {
         // if the readiness event is a false positive.
         match stream.try_write(b"hello world") {
             Ok(n) => {
-                // break;
+                break;// break or this will send all the time
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
@@ -101,16 +104,16 @@ async fn write() -> Result<(), Box<dyn Error>> {
 
 async fn read_write() -> Result<(), Box<dyn Error>> {
     let mut socket = TcpStream::connect("127.0.0.1:6123").await?;
-    let (r,w)=socket.split();
-    let stream  = FramedRead::new(r,BytesCodec::new());
-    let sink = FramedWrite::new(w,BytesCodec::new());
-    let st =stream.filter_map(|i| match i {
+    let (r, w) = socket.split();
+    let stream = FramedRead::new(r, BytesCodec::new());
+    let sink = FramedWrite::new(w, BytesCodec::new());
+    let st = stream.filter_map(|i| match i {
         Ok(i) => future::ready(Some(i.freeze())),
         Err(e) => {
             future::ready(None)
         }
     });
-        // .map(Ok);
+    // .map(Ok);
     // match future::join(sink.send_all(&mut stdin), stdout.send_all(&mut stream)).await {
     //     (Err(e), _) | (_, Err(e)) => Err(e.into()),
     //     _ => ()
@@ -120,8 +123,8 @@ async fn read_write() -> Result<(), Box<dyn Error>> {
 
 pub async fn connect(
     addr: &SocketAddr,
-    mut stdin: impl Stream<Item = Result<Bytes, io::Error>> + Unpin,
-    mut stdout: impl Sink<Bytes, Error = io::Error> + Unpin,
+    mut stdin: impl Stream<Item=Result<Bytes, io::Error>> + Unpin,
+    mut stdout: impl Sink<Bytes, Error=io::Error> + Unpin,
 ) -> Result<(), Box<dyn Error>> {
     let mut stream = TcpStream::connect(addr).await?;
     let (r, w) = stream.split();
@@ -143,5 +146,4 @@ pub async fn connect(
         (Err(e), _) | (_, Err(e)) => Err(e.into()),
         _ => Ok(()),
     }
-
 }
